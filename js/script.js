fd2ce74537d73a513c60d65a92b2ce260a26040a
YeJa <abdilmanovyernar@gmail.com>
2019-08-19 12:20:10 +0600

    pdf 2.2

--- js/script.js
+++ js/script.js
@@ -872,9 +872,33 @@ require([
                         //------------------------
 
                         console.log(data);
-                        let pdfWindow = window.open("");
-                        pdfWindow.document.write("<html<head><title>"+data.file_name+"</title><style>body{margin: 0px;}</style></head>");
-                        pdfWindow.document.write("<body><embed width='100%' height='100%' src='data:application/pdf;base64, " + encodeURI(data.file)+"#toolbar=0&navpanes=0&scrollbar=0'></embed></body></html>");
+
+                        const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
+                          const byteCharacters = atob(b64Data);
+                          const byteArrays = [];
+
+                          for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+                            const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+                            const byteNumbers = new Array(slice.length);
+                            for (let i = 0; i < slice.length; i++) {
+                              byteNumbers[i] = slice.charCodeAt(i);
+                            }
+
+                            const byteArray = new Uint8Array(byteNumbers);
+                            byteArrays.push(byteArray);
+                          }
+
+                          const blob = new Blob(byteArrays, {type: contentType});
+                          return blob;
+                        }
+                        const contentType = 'application/pdf';
+
+                        const blob = b64toBlob(data.file, contentType);
+                        const blobUrl = URL.createObjectURL(blob);
+
+                        window.location = blobUrl;
+
 
                       }else{
                         alert("Формат файла не поддерживается");

a6ac8e0f510dab57f3458f909f504994c93c4ef9
YeJa <abdilmanovyernar@gmail.com>
2019-08-19 11:59:49 +0600

    pdf 2.1

--- js/script.js
+++ js/script.js
@@ -870,10 +870,10 @@ require([
                         // var layer = $(win.document);
 
                         //------------------------
-                        
-                        console.log(data.file);
+
+                        console.log(data);
                         let pdfWindow = window.open("");
-                        pdfWindow.document.write("<html<head><title>"+data.file_name+"</title><style>body{margin: 0px;}iframe{border-width: 0px;}</style></head>");
+                        pdfWindow.document.write("<html<head><title>"+data.file_name+"</title><style>body{margin: 0px;}</style></head>");
                         pdfWindow.document.write("<body><embed width='100%' height='100%' src='data:application/pdf;base64, " + encodeURI(data.file)+"#toolbar=0&navpanes=0&scrollbar=0'></embed></body></html>");
 
                       }else{

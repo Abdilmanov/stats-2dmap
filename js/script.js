3f07a33ac9e63640d81fdad1b0a8ab1bfc2e6ed0
YeJa <abdilmanovyernar@gmail.com>
2019-08-19 10:11:16 +0600

    pdf 1.7

--- js/script.js
+++ js/script.js
@@ -871,7 +871,15 @@ require([
 
                         //------------------------
                         console.log(data.file);
-                        window.open("data:application/octet-stream;charset=utf-16le;base64,"+data.file);
+                        var byteCharacters = atob(data.file);
+                        var byteNumbers = new Array(byteCharacters.length);
+                        for (var i = 0; i < byteCharacters.length; i++) {
+                          byteNumbers[i] = byteCharacters.charCodeAt(i);
+                        }
+                        var byteArray = new Uint8Array(byteNumbers);
+                        var file = new Blob([byteArray], { type: 'application/pdf;base64' });
+                        var fileURL = URL.createObjectURL(file);
+                        window.open(fileURL);
 
                       }else{
                         alert("Формат файла не поддерживается");
